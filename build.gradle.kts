45b3426d6d2865fb2db96b1df5ced3666a03ddfb
cayceevans213 <cevangelista2021@student.nbscollege.edu.ph>
2024-04-20 00:01:50 +0800

    asd

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+// Top-level build file where you can add configuration options common to all subprojects/modules.
+
 buildscript {
     ext {
         val compose_version = "1.1.0-rc01"
@@ -6,15 +8,29 @@ buildscript {
         google()
         mavenCentral()
     }
+
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.4")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
     }
 }
 
-tasks.register("clean", Delete::class) {
+tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
+}
+
+/**
+ * Run ./gradlew dependencyUpdates to check for new updates
+ * in dependencies used.
+ * More info at: https://github.com/ben-manes/gradle-versions-plugin
+ */
+
+/**
+ * Decides if this version is stable or not.
+ */
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return !isStable
 }
